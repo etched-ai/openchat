bebbea41267ee98009b7e1f7b4eb20158112665f
zolinthecow <colinzhao777@gmail.com>
2024-08-09 13:50:11 -0700

    install tanstack router

--- apps/ui/src/main.tsx
+++ apps/ui/src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import ReactDOM from 'react-dom/client';
 
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-);
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router;
+    }
+}
+
+// biome-ignore lint/style/noNonNullAssertion: The root definitely exists
+const rootElement = document.getElementById('root')!;
+if (!rootElement.innerHTML) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <StrictMode>
+            <RouterProvider router={router} />
+        </StrictMode>,
+    );
+}
--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -1,10 +1,11 @@
 import path from 'node:path';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [TanStackRouterVite(), react()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
