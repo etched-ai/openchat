47d3936d9b1423a48d852ffa53d4fcd7e1a8fd8d
zolinthecow <colinzhao777@gmail.com>
2024-08-11 00:11:21 -0700

    show submit button when there is content

--- apps/ui/src/components/Milkdown/Milkdown.tsx
+++ apps/ui/src/components/Milkdown/Milkdown.tsx
@@ -1,18 +1,12 @@
-import {
-    Editor,
-    defaultValueCtx,
-    editorViewCtx,
-    rootCtx,
-} from '@milkdown/core';
+import { Editor, defaultValueCtx, rootCtx } from '@milkdown/core';
 import { history } from '@milkdown/plugin-history';
+import { listener } from '@milkdown/plugin-listener';
 import { commonmark } from '@milkdown/preset-commonmark';
 import { gfm } from '@milkdown/preset-gfm';
-import { TextSelection } from '@milkdown/prose/state';
-import { Milkdown, MilkdownProvider, useEditor } from '@milkdown/react';
+import { Milkdown, useEditor } from '@milkdown/react';
 import { nord } from '@milkdown/theme-nord';
 import { placeholder, placeholderCtx } from 'milkdown-plugin-placeholder';
 import type React from 'react';
-import { useCallback, useEffect } from 'react';
 import codeBlockSyntaxPlugin from './milkdownPlugins/codeBlockSyntaxPlugin';
 import headerSyntaxPlugin from './milkdownPlugins/headerSyntaxPlugin';
 import inlineCodePlugin from './milkdownPlugins/inlineCodeSyntaxPlugin';
@@ -26,6 +20,7 @@ export const MilkdownEditor: React.FC = () => {
                 ctx.set(defaultValueCtx, '');
                 ctx.set(placeholderCtx, 'How can Charlie help you today?');
             })
+            .use(listener)
             .use(commonmark)
             .use(gfm)
             .use(history)
@@ -33,25 +28,8 @@ export const MilkdownEditor: React.FC = () => {
             .use([headerSyntaxPlugin, codeBlockSyntaxPlugin, inlineCodePlugin]),
     );
 
-    useEffect(() => {
-        const editor = get();
-        if (editor) {
-            editor.action((ctx) => {
-                const view = ctx.get(editorViewCtx);
-                const { state } = view;
-                if (state.selection?.empty) {
-                    const selection = TextSelection.create(state.doc, 1);
-                    view.focus();
-                    view.dispatch(state.tr.setSelection(selection));
-                } else {
-                    view.focus();
-                }
-            });
-        }
-    }, [get]);
-
     return (
-        <div className="w-full h-full">
+        <div className="w-full h-full p-2">
             <Milkdown />
         </div>
     );
--- apps/ui/src/routes/index.lazy.tsx
+++ apps/ui/src/routes/index.lazy.tsx
@@ -1,51 +1,129 @@
 import { MilkdownEditor } from '@/components/Milkdown/Milkdown';
+import { Button } from '@/components/ui/button';
 import { editorViewCtx } from '@milkdown/core';
+import type { Ctx } from '@milkdown/ctx';
 import { TextSelection } from '@milkdown/prose/state';
 import { MilkdownProvider, useInstance } from '@milkdown/react';
-import { createLazyFileRoute } from '@tanstack/react-router';
+import { getMarkdown } from '@milkdown/utils';
+import { createLazyFileRoute, useRouter } from '@tanstack/react-router';
+import { ArrowUp } from 'lucide-react';
+import { useCallback, useEffect, useState } from 'react';
 
 export const Route = createLazyFileRoute('/')({
     component: Index,
 });
 
 function Index() {
+    const router = useRouter();
+
+    const handleSubmit = () => {
+        console.log('Submitting!');
+    };
+
     return (
         <div className="w-full h-full justify-center items-center flex flex-col p-2">
             <h1 className="text-4xl">Good afternoon, Colin :)</h1>
             <MilkdownProvider>
-                <InputBox />
+                <InputBox handleSubmit={handleSubmit} />
             </MilkdownProvider>
         </div>
     );
 }
 
-const InputBox: React.FC = () => {
+type InputBoxProps = {
+    handleSubmit: () => void;
+};
+const InputBox: React.FC<InputBoxProps> = ({ handleSubmit }) => {
     const [loading, getEditor] = useInstance();
+    const editorAction = useCallback(
+        (fn: (ctx: Ctx) => void) => {
+            if (loading) return null;
+            return getEditor().action(fn);
+        },
+        [loading, getEditor],
+    );
 
-    function focusOnEditor() {
-        const editor = getEditor();
-        if (editor) {
-            editor.action((ctx) => {
-                const view = ctx.get(editorViewCtx);
-                const { state } = view;
-                if (state.selection?.empty) {
-                    const selection = TextSelection.create(state.doc, 1);
-                    view.focus();
-                    view.dispatch(state.tr.setSelection(selection));
-                } else {
-                    view.focus();
-                }
-            });
-        }
-    }
+    const [inputHasContent, setInputHasContent] = useState(false);
+
+    const handleContentUpdated = useCallback(() => {
+        editorAction((ctx) => {
+            const md = getMarkdown()(ctx);
+            setInputHasContent(md.length > 0);
+        });
+    }, [editorAction]);
+
+    const handleKeyUp = useCallback(
+        (e: KeyboardEvent) => {
+            if (
+                e.key === 'Enter' &&
+                !e.shiftKey &&
+                !e.ctrlKey &&
+                !e.altKey &&
+                !e.metaKey
+            ) {
+                e.preventDefault();
+                handleSubmit();
+            }
+            handleContentUpdated();
+        },
+        [handleContentUpdated, handleSubmit],
+    );
+
+    useEffect(() => {
+        handleContentUpdated();
+
+        editorAction((ctx) => {
+            // I know there is a milkdown listener plugin. However, it's really slow.
+            // It's much faster to just listen to the key events themselves and press.
+            const view = ctx.get(editorViewCtx);
+            view.dom.addEventListener('keyup', handleKeyUp);
+
+            return () => {
+                view.dom.removeEventListener('keyup', handleKeyUp);
+            };
+        });
+    }, [editorAction, handleContentUpdated, handleKeyUp]);
+
+    const focusOnEditor = useCallback(() => {
+        editorAction((ctx) => {
+            const view = ctx.get(editorViewCtx);
+            const { state } = view;
+            if (!state.selection) {
+                const selection = TextSelection.create(state.doc, 1);
+                view.focus();
+                view.dispatch(state.tr.setSelection(selection));
+            } else {
+                view.focus();
+            }
+        });
+    }, [editorAction]);
+
+    useEffect(() => {
+        focusOnEditor();
+    }, [focusOnEditor]);
 
     return (
         // biome-ignore lint/a11y/useKeyWithClickEvents: This is technically a text input
         <div
-            className="flex items-start w-full max-w-[36rem] min-h-24 bg-muted rounded-md p-4 border-[0.5px] border-border border-opacity-30 mt-8"
+            className="flex items-start w-full max-w-[36rem] min-h-24 max-h-36 overflow-y-scroll bg-muted rounded-md p-2 border-[0.5px] border-border border-opacity-30 mt-8"
             onClick={() => focusOnEditor()}
         >
             <MilkdownEditor />
+            {inputHasContent && <SubmitButton onSubmit={handleSubmit} />}
         </div>
+    );
+};
+
+type SubmitButtonProps = {
+    onSubmit: () => void;
+};
+const SubmitButton: React.FC<SubmitButtonProps> = ({ onSubmit }) => {
+    return (
+        <Button
+            className="w-8 h-8 p-0 rounded-xl bg-primary text-primary-foreground flex items-center justify-center"
+            onClick={onSubmit}
+        >
+            <ArrowUp className="w-4 h-4" />
+        </Button>
     );
 };
