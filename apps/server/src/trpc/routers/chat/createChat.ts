1b5245b01c3dcace97b34430fd1048f2f490929f
zolinthecow <colinzhao777@gmail.com>
2024-08-14 12:40:54 -0700

    create chat uses slonik

--- apps/server/src/trpc/routers/chat/createChat.ts
+++ apps/server/src/trpc/routers/chat/createChat.ts
@@ -1,15 +1,25 @@
-import type { DBChat } from '@repo/db';
-import { DateTime } from 'luxon';
+// Creates a new empty chat in the DB. Must call this before sending any messages, even from the home
+// page.
+
+import { DBChatSchema } from '@repo/db';
+import { sql } from 'slonik';
 import { ulid } from 'ulid';
 import { publicProcedure } from '../../trpc';
 
 export const createChat = publicProcedure.mutation(async ({ ctx }) => {
-    const newChat: DBChat = {
-        id: ulid(),
-        userID: ctx.user.id,
-        previewName: '',
-        createdAt: DateTime.now().toJSDate(),
-        updatedAt: DateTime.now().toJSDate(),
-    };
+    const newChat = await ctx.dbPool.one(sql.type(DBChatSchema)`
+        INSERT INTO "Chat" (
+            id,
+            "userID",
+            "previewName",
+            "createdAt",
+            "updatedAt"
+        ) VALUES (
+            ${ulid()},
+            ${ctx.user.id},
+            ${''}, -- Empty string for previewName
+        )
+        RETURNING *
+    `);
     return newChat;
 });
