76f3c420f22ad48736e9f55b0cf797f7fea6afea
zolinthecow <colinzhao777@gmail.com>
2024-08-24 11:46:12 -0700

    extract name and link supabase auth properly

--- apps/ui/src/routes/index.lazy.tsx
+++ apps/ui/src/routes/index.lazy.tsx
@@ -1,8 +1,10 @@
 import InputBox from '@/components/InputBox';
 import { Button } from '@/components/ui/button';
 import { queryClient } from '@/lib/reactQuery';
+import { supabase } from '@/lib/supabase';
 import { type TRPCOutputs, trpc } from '@/lib/trpc';
 import type { AsyncGeneratorYieldType } from '@/lib/utils';
+import type { Session } from '@supabase/supabase-js';
 import {
     createLazyFileRoute,
     useRouteContext,
@@ -10,15 +12,32 @@ import {
 } from '@tanstack/react-router';
 import { getQueryKey } from '@trpc/react-query';
 import { DateTime } from 'luxon';
+import { useTransition } from 'react';
 
 export const Route = createLazyFileRoute('/')({
     component: Index,
 });
 
+function maybeGetName(session: Session | null): null | string {
+    if (!session) return null;
+    const name =
+        session.user.identities?.[0].identity_data?.full_name ||
+        session.user.identities?.[0].identity_data?.full_name;
+    if (!name) return null;
+    const nameParts = name.split(' ');
+    if (nameParts.length === 1) {
+        return nameParts[0];
+    }
+    return nameParts.slice(0, -1).join(' ');
+}
+
 function Index() {
     const router = useRouter();
     const context = useRouteContext({ from: '/' });
+    const name = maybeGetName(context.session);
 
+    const [pendingSubmitChatMessage, startSubmitChatMessageTransition] =
+        useTransition();
     const createChatMutation = trpc.chat.create.useMutation();
     const generateResponseMutation =
         trpc.chatMessages.generateResponse.useMutation();
@@ -34,50 +53,67 @@ function Index() {
         }
     };
 
-    const handleSubmit = async (text: string): Promise<void> => {
-        // Create the chat and send the first message
-        const createChatResp = await createChatMutation.mutateAsync({
-            initialMessage: text,
-        });
-        await queryClient.invalidateQueries({
-            queryKey: getQueryKey(trpc.chat.infiniteList, undefined, 'any'),
-        });
-        const generateResponseResp = await generateResponseMutation.mutateAsync(
-            {
-                messageID: createChatResp.messages[0].id,
-            },
-        );
+    const handleSubmit = (text: string): void =>
+        startSubmitChatMessageTransition(async () => {
+            // Create the chat and send the first message
+            const createChatResp = await createChatMutation.mutateAsync({
+                initialMessage: text,
+            });
+            await queryClient.invalidateQueries({
+                queryKey: getQueryKey(trpc.chat.infiniteList, undefined, 'any'),
+            });
+            const generateResponseResp =
+                await generateResponseMutation.mutateAsync({
+                    messageID: createChatResp.messages[0].id,
+                });
 
-        // Convert the async generator into a readable stream
-        const stream = new ReadableStream<
-            AsyncGeneratorYieldType<
-                TRPCOutputs['chatMessages']['generateResponse']
-            >
-        >({
-            async start(controller) {
-                for await (const chunk of generateResponseResp) {
-                    controller.enqueue(chunk);
-                }
-                controller.close();
-            },
+            // Convert the async generator into a readable stream
+            const stream = new ReadableStream<
+                AsyncGeneratorYieldType<
+                    TRPCOutputs['chatMessages']['generateResponse']
+                >
+            >({
+                async start(controller) {
+                    for await (const chunk of generateResponseResp) {
+                        controller.enqueue(chunk);
+                    }
+                    controller.close();
+                },
+            });
+
+            // Now the stream gets passed into the router context so you can just keep
+            // reading from it without worrying about resetting it
+
+            context.initialChatStream = stream;
+            router.navigate({
+                from: '/',
+                to: '/c/$chatID',
+                params: {
+                    chatID: createChatResp.id,
+                },
+            });
         });
 
-        // Now the stream gets passed into the router context so you can just keep
-        // reading from it without worrying about resetting it
+    const [pendingLogin, startLoginTransition] = useTransition();
 
-        context.initialChatStream = stream;
-        router.navigate({
-            from: '/',
-            to: '/c/$chatID',
-            params: {
-                chatID: createChatResp.id,
-            },
+    const handleLogin = (): void =>
+        startLoginTransition(async () => {
+            const { data, error } = await supabase.auth.linkIdentity({
+                provider: 'google',
+            });
+            if (error) {
+                console.error(error);
+                return;
+            }
+            console.log('SUCCESS', data);
         });
-    };
 
     return (
         <div className="w-full h-full justify-center items-center flex flex-col p-2">
-            <h1 className="text-4xl">Good {getGreeting()}, Colin :)</h1>
+            <h1 className="text-4xl">
+                Good {getGreeting()}
+                {name != null ? `, ${name}` : ''} :)
+            </h1>
             <div className="rounded-md overflow-hidden flex w-[36rem] min-h-28 max-h-36 border-[0.5px] border-border border-opacity-30 mt-8">
                 <InputBox
                     handleSubmit={handleSubmit}
@@ -85,7 +121,9 @@ function Index() {
                 />
             </div>
             {(!context.session || context.session.user.is_anonymous) && (
-                <Button className="w-72 mt-6">Log in with Google</Button>
+                <Button onClick={handleLogin} className="w-72 mt-6">
+                    Log in with Google
+                </Button>
             )}
         </div>
     );
